217eae5cc428c0994a266f22ccedb73619afa6d6
Masaki <masaki.yoshiiwa@gmail.com>
2023-10-10 10:57:01 +0900

    chore: update bio

--- src/i18n/en/index.ts
+++ src/i18n/en/index.ts
@@ -4,7 +4,7 @@ const en = {
   hello: `Hello World`,
   name: `Masaki Yoshiiwa`,
   job: `Software developer, full stack developer, working primarily on web and mobile apps.`,
-  introduce: `Passionate about solving interesting problems, and finding new technologies to learn.`,
+  introduce: `Passionate about solving interesting problems and finding new technologies. Loves automation, design, and writing code.`,
   resumeUrl: `https://github.com/qlawmarq/resume/blob/master/MasakiYoshiiwa_Resume.pdf`,
 } satisfies BaseTranslation;
 
