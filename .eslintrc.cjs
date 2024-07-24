ad7abb320124f448833576ba49f70dad21c6055e
Masaki Yoshiiwa <masaki.yoshiiwa@gmail.com>
2024-07-24 13:33:12 +0900

    chore: update 2024-07 (#11)
    * chore: update contents and deps
    
    * feat: build check ci

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,7 +24,8 @@ module.exports = {
   ],
   rules: {
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["error"],
+    "no-undef": "off",
+    "@typescript-eslint/no-unused-vars": ["off"],
   },
   parserOptions: {
     sourceType: "module",
