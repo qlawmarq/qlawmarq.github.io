affeed03a71240cc648e1cb0622e05e820820e38
Masaki Yoshiiwa <masaki.yoshiiwa@gmail.com>
2024-10-14 16:50:09 +0900

    Revert "feat: add blog rss feed (#13)"
    This reverts commit 6127895852be02a3067e17038bb145e89fc98d87.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,12 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  extends: ["plugin:svelte/recommended", "prettier"],
-  parserOptions: {
-    project: "./tsconfig.json",
-    extraFileExtensions: [".svelte"], // This is a required setting in `@typescript-eslint/parser` v4.24.0.
-  },
+  extends: [
+    "plugin:svelte/recommended",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
   plugins: ["@typescript-eslint"],
   ignorePatterns: ["*.cjs"],
   overrides: [
@@ -13,7 +14,11 @@ module.exports = {
       files: ["*.svelte"],
       parser: "svelte-eslint-parser",
       parserOptions: {
-        parser: "@typescript-eslint/parser",
+        parser: {
+          // Specify a parser for each lang.
+          ts: "@typescript-eslint/parser",
+          typescript: "@typescript-eslint/parser",
+        },
       },
     },
   ],
--- src/types/rss.ts
+++
@@ -1,6 +0,0 @@
-export type RSSItem = {
-  title: string;
-  link: string;
-  description: string;
-  pubDate: string;
-};
