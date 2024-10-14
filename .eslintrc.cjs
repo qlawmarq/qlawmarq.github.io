6127895852be02a3067e17038bb145e89fc98d87
Masaki Yoshiiwa <masaki.yoshiiwa@gmail.com>
2024-10-14 16:42:00 +0900

    feat: add blog rss feed (#13)
    * feat: add blog rss feed
    
    * chore: update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,11 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  extends: [
-    "plugin:svelte/recommended",
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
+  extends: ["plugin:svelte/recommended", "prettier"],
+  parserOptions: {
+    project: "./tsconfig.json",
+    extraFileExtensions: [".svelte"], // This is a required setting in `@typescript-eslint/parser` v4.24.0.
+  },
   plugins: ["@typescript-eslint"],
   ignorePatterns: ["*.cjs"],
   overrides: [
@@ -14,11 +13,7 @@ module.exports = {
       files: ["*.svelte"],
       parser: "svelte-eslint-parser",
       parserOptions: {
-        parser: {
-          // Specify a parser for each lang.
-          ts: "@typescript-eslint/parser",
-          typescript: "@typescript-eslint/parser",
-        },
+        parser: "@typescript-eslint/parser",
       },
     },
   ],
